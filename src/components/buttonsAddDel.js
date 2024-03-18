937ae5385fe096e9c4367c5a7835295cefd55f18
Wellison Silva <wonsilva100@gmail.com>
2024-03-18 13:17:06 -0300

    add buttons add product

---
+++ src/components/buttonsAddDel.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, Pressable, StyleSheet } from "react-native";
+
+export default props => {
+    return (
+        <View>
+            <Pressable>-</Pressable>
+            <Pressable>+</Pressable>
+        </View>
+    )
+}
+
--- src/screens/AddOrders.js
+++ src/screens/AddOrders.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Pressable,  } from "react-native";
+import { View, Text, StyleSheet, Pressable, ScrollView,  } from "react-native";
 
 import ProductList from "../components/ProductList";
 
@@ -8,10 +8,25 @@ export default class AddOrders extends Component {
         return (
             <View style={styles.container}>
                 <View styles>
+                    <ScrollView>
                     <ProductList product='teste1' value={30.00}/>
                     <ProductList product='teste2' value={30.00}/>
-                    <ProductList product='teste3' value={30.00}/>
-                    <ProductList product='teste4' value={30.00}/>
+                    <ProductList product='teste2' value={30.00}/>
+                    <ProductList product='teste2' value={30.00}/>
+                    <ProductList product='teste2' value={30.00}/>
+                    <ProductList product='teste2' value={30.00}/>
+                    <ProductList product='teste2' value={30.00}/>
+                    <ProductList product='teste2' value={30.00}/>
+                    <ProductList product='teste2' value={30.00}/>
+                    <ProductList product='teste2' value={30.00}/>
+                    <ProductList product='teste2' value={30.00}/>
+                    <ProductList product='teste2' value={30.00}/>
+                    <ProductList product='teste2' value={30.00}/>
+                    <ProductList product='teste2' value={30.00}/>
+                    <ProductList product='teste2' value={30.00}/>
+                    <ProductList product='teste2' value={30.00}/>
+                    <ProductList product='teste2' value={30.00}/>
+                    </ScrollView>
                 </View>
             </View>
         )
